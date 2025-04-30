43299e0ebee88d60c67b35226a8a0a6517425941
idriouic <ilias.driouich@amadeus.com>
2025-04-30 07:29:36 +0200

    updated agents

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setup(
     name='privatetestsetgenerationforLLMeval',
-    version='1.4',
+    version='1.5',
     author='Ilias',
     author_email='ilias.driouich@amadeus.com',
     description='A package for generating evaluation set for LLM-based chatbots in a diverse and private manner',
